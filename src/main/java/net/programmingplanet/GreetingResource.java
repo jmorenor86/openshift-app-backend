ce4ea67a2fa8dbd008f9b5baa539c8dac459b29a
John <john.moreno@ibm.com>
2025-03-03 12:15:45 +0100

    feat: change response to json

--- src/main/java/net/programmingplanet/GreetingResource.java
+++ src/main/java/net/programmingplanet/GreetingResource.java
@@ -4,13 +4,22 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        return Response.ok(new Message("Hello from Quarkus REST")).build();
+    }
+
+    public static class Message {
+        public String message;
+
+        public Message(String message) {
+            this.message = message;
+        }
     }
 }
