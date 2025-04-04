f5577d006d7eafa519be3b48d8bb50d46da23dac
John <john.moreno@ibm.com>
2025-04-04 16:20:17 +0200

    fix: add into containerfile the copy of reflect config

--- src/main/java/net/programmingplanet/GreetingResource.java
+++ src/main/java/net/programmingplanet/GreetingResource.java
@@ -6,7 +6,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/hello-test")
 public class GreetingResource {
 
     @GET
